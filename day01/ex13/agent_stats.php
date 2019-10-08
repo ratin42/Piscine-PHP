81080f397f6c617cebca15a3223cccfea7db3c4e
Raphael TIN <ratin@e3r4p9.42.fr>
2019-10-08 21:05:48 +0200

    rules

--- day01/ex13/agent_stats.php
+++ day01/ex13/agent_stats.php
@@ -1,5 +1,14 @@
 #!/usr/bin/php
-
 <?php 
 
+function get_user($line)
+{
+	return (substr($line, 0, strpos($line, ";")));
+}
+
+while ($line = fgets(STDIN))
+{
+	echo get_user($line) . "|" . PHP_EOL;
+	echo $line;
+}
 ?>
